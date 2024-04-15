b1a9b42df7937ac5c07d8142ef687c7d70d0d787
jparouca <joaopearoucaa@gmail.com>
2024-04-15 20:02:38 -0300

    media slice

--- main.ts
+++ main.ts
@@ -47,13 +47,14 @@ function Media() {
   const label = Utils.watch("", mpris, "player-changed", () => {
     if (mpris.players[0]) {
       const { track_artists, track_title } = mpris.players[0]
-      return `${track_artists.join(", ")} - ${track_title}`
+      return `${track_artists.join(", ")} - ${track_title.slice(0, 16)}`
     }
-    return ""
+    return "-"
   })
 
   return Widget.Button({
     class_name: "media",
+    onMiddleClick: self => self.revealChild = !self.revealChild,
     on_primary_click: () => mpris.getPlayer("")?.playPause(),
     on_scroll_up: () => mpris.getPlayer("")?.next(),
     on_scroll_down: () => mpris.getPlayer("")?.previous(),
