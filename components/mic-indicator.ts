793d12c3828fc02b7ef45c9bd6f9a0e73afca73c
jparouca <joaopearoucaa@gmail.com>
2024-04-16 23:15:14 -0300

    custom icons

--- components/mic-indicator.ts
+++ components/mic-indicator.ts
@@ -3,7 +3,7 @@ const audio = await Service.import('audio')
 export const MicIndicator = Widget.Button({
   className: 'microphone',
   child: Widget.Icon().hook(audio.microphone, self => {
-    self.icon = audio.microphone.is_muted ? 'microphone-disabled-symbolic' : 'audio-input-microphone-symbolic'
+    self.icon = audio.microphone.is_muted ? '../icons/microphone-slash-fill.svg' : '../icons/microphone-fill.svg'
     self.toggleClassName('microphone-muted', audio?.microphone.is_muted)
   })
 })
