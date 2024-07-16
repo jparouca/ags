57cdbfe768d3e445e2d7f98a1374b08b11abd174
jparouca <joaopearoucaa@gmail.com>
2024-07-16 18:08:24 -0300

    clock format

--- components/clock.ts
+++ components/clock.ts
@@ -1,22 +1,42 @@
 const { GLib } = imports.gi;
 import popupWindow from './popup-window';
 
-export const Clock = () =>
-  Widget.Button({
-    vpack: 'center',
-    className: 'clock',
+export const Clock = () => {
+  const dateFormat = '%a %d %b';
+  const timeFormat = '%H:%M'
+
+  const updateDate = () => GLib.DateTime.new_now_local().format(dateFormat);
+  const updateTime = () => GLib.DateTime.new_now_local().format(timeFormat)
+
+  const dateLabel = Widget.Label({
+    className: 'bar-date',
+    label: updateDate(),
+    setup: (self) =>
+      self.poll(60000, (label) => {
+        label.label = updateDate();
+      }),
+  });
+
+  const timeButton = Widget.Button({
+    className: 'bar-time',
     onClicked: () => App.toggleWindow('clock-window'),
-    tooltipText: GLib.DateTime.new_now_local().format('%c'),
     child: Widget.Label({
-      className: 'bar-time',
-      label: GLib.DateTime.new_now_local().format('%a, %H:%M %p'),
+      label: updateTime(),
       setup: (self) =>
         self.poll(5000, (label) => {
-          label.label =
-            GLib.DateTime.new_now_local().format('%a, %H:%M %p') ?? '--:--';
+          label.label = updateTime();
         }),
-    }),
-  });
+    })
+  })
+
+  return (
+    Widget.Box({
+      className: 'clock',
+      children: [dateLabel, timeButton],
+      tooltipText: GLib.DateTime.new_now_local().format('%c'),
+    })
+  );
+}
 
 export const ClockWindow = () => {
   return popupWindow({
